43c78afbf9e4245530805554c71156463ef11ce6
Janet Mendez <janetmendez.03@gmail.com>
2019-07-01 21:10:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
 # My Code here....
+def map_to_negativize(arr)
+  arr.map {|n| n * -1}
+end
+
+def map_to_no_change(arr)
+  arr
+end
+
+def map_to_double(arr)
+  arr.map {|n| n * 2}
+end
+
+def map_to_square(arr)
+  arr.map {|n| n ** 2}
+end
+
+def reduce_to_total(arr, start = 0)
+  arr.reduce(start) {|total, num| total + num}
+end
